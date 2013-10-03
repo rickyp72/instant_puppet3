fbdc9166b7982aea693bb2860620601a69330fd3
root <root@rhel6puppet.localdomain>
2013-10-03 09:41:16 -0700

    initial commit

---
+++ manifests/site.pp
@@ -0,0 +1,15 @@
+node default {
+	class { 'puppet': }
+}
+
+node 'rhel6puppet.localdomain' inherits default {
+	package {'puppet-server':
+		ensure => latest,
+		notify => Service['puppetmaster'],
+	}
+	service { 'puppetmaster':
+		ensure => running,
+		enable => true,
+		subscribe => Package['puppet-server'],
+	}
+}
